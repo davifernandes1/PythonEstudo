8b1106e80fdeaaf6c5e633a72f558066833d70b7
Davi <99411194+davifernandes1@users.noreply.github.com>
2024-09-30 12:53:01 -0300

    Update duracao.py

--- estrutura-sequencial/duracao.py
+++ estrutura-sequencial/duracao.py
@@ -3,7 +3,6 @@ Fazer um programa para ler uma duração de tempo em segundos,
 daí imprimir na tela esta duração no formato horas:minutos:segundos.
 '''
 
-duracao: int; horas: int; minutos: int; segundos: int; resto: int
 
 duracao = int(input("Digite a duracao em segundos: "))
 
