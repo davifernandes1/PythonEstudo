cbf11b9bcabe5da96a3660f070872bf0d0e5f3cf
Davi <99411194+davifernandes1@users.noreply.github.com>
2024-09-30 12:51:32 -0300

    Update aumento.py

--- estrutura-condicional/aumento.py
+++ estrutura-condicional/aumento.py
@@ -12,51 +12,21 @@ Acima de R$ 3000.00 ate R$ 8000.000 - aumento de 10
 Acima de R$ 8000.00 - aumento de 5%
 '''
 
-salario: float; novo_salario: float; aumento: float; porcentagem: float
+salario = float(input("Digite o salário do funcionario: R$ "))
 
-salario = float(input("Digite o salario da pessoa: "))
-
-if salario <= 1000:
-    porcentagem = 20
-elif salario <= 3000:
-    porcentagem = 15
-elif salario <= 8000:
-    porcentagem = 10
+if salario <= 1000.00:
+    percentual = 20
+elif salario <= 3000.00:
+    percentual = 15
+elif salario <= 8000.00:
+    percentual = 10
 else:
-    porcentagem = 5
+    percentual = 5
 
-aumento = salario * porcentagem / 100
+aumento = salario * (percentual / 100)
 novo_salario = salario + aumento
 
-print(f"Novo salario = R${novo_salario:.2f}")
-print(f"Aumento = R${aumento:.2f}")
-print(f"Porcentagem = {porcentagem}%")
-
-'''
-salario: float; novosalario: float; aumento: float
-porcentagem: int
-
-salario = float(input("Digite o salario da pessoa: "))
-
-if salario <= 1000.0:
-	porcentagem = 20
-	aumento = salario * porcentagem / 100
-	novosalario = salario + aumento
-elif salario <= 3000.0:
-	porcentagem = 15
-	aumento = salario * porcentagem / 100
-	novosalario = salario + aumento
-elif salario <= 8000.0:
-	porcentagem = 10
-	aumento = salario * porcentagem / 100
-	novosalario = salario + aumento
-else:
-	porcentagem = 5
-	aumento = salario * porcentagem / 100
-	novosalario = salario + aumento
-
-print(f"Novo salario = R$ {novosalario:.2f}")
-print(f"Aumento = R$ {aumento:.2f}")
-print(f"Porcentagem = {porcentagem} %")
-'''
+print(f"Percentual de aumento aplicado: {percentual}%")
+print(f"Valor do aumento: R$ {aumento}")
+print(f"Novo salário: R$ {novo_salario}")
 
